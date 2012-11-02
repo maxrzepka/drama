f56f11267e6813d1dc685fff97c87077bc5a249a
Maximilien Rzepka <maximilien.rzepka@gmail.com>
2012-11-02 09:29:29 +0100

    fix java.lang.NoSuchMethodError: org.slf4j.spi.LocationAwareLogger.log

--- project.clj
+++ project.clj
@@ -5,7 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring "1.1.0"]
+                 ;;add explicitly slf4j dependencies to fix java.lang.NoSuchMethodError: org.slf4j.spi.LocationAwareLogger.log
+                 [org.slf4j/slf4j-log4j12 "1.6.1"]
+                 [org.slf4j/slf4j-api "1.6.1"]
                  [enlive "1.0.1"]
-                 [net.cgrand/moustache "1.1.0" :exclusions [ring]]
+                 [net.cgrand/moustache "1.1.0" ]
                  [cascalog "1.9.0"]]
   :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]]}})
