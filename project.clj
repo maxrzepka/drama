be81bc88e85ce20ac6d961aa9ab004c822770e7f
Maximilien Rzepka <maximilien.rzepka@gmail.com>
2012-11-04 18:03:32 +0100

    upgrade deps

--- project.clj
+++ project.clj
@@ -4,11 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [ring "1.1.0"]
+                 [ring "1.1.6"]
                  ;;add explicitly slf4j dependencies to fix java.lang.NoSuchMethodError: org.slf4j.spi.LocationAwareLogger.log
                  [org.slf4j/slf4j-log4j12 "1.6.1"]
                  [org.slf4j/slf4j-api "1.6.1"]
                  [enlive "1.0.1"]
-                 [net.cgrand/moustache "1.1.0" ]
-                 [cascalog "1.9.0"]]
-  :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]]}})
+                 [net.cgrand/moustache "1.1.0"]
+                 [cascalog "1.10.0"]
+                 ]
+  :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]]}}
+  :min-lein-version "2.0.0"
+  :main drama.act3)
